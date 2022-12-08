63654bcd5794f1becefcd959f14c7b72fd1888c1
McVyp <93302780+McVyp@users.noreply.github.com>
2022-12-08 22:29:48 +0545

    added navigation animation

--- components/header.tsx
+++ components/header.tsx
@@ -16,33 +16,39 @@ export const Header = () => {
             <Link className='flex items-center text-md' href='/'>
                 <Logo className='w-[1.8rem] h-[1.8rem] mr-4'/> Linear
             </Link>
-            <nav className={classNames('h-[calc(100vh_-_var(--navigation-height))] md:block fixed md:relative top-navigation-height left-0 bg-background w-full overflow-auto md:h-auto md:w-auto md:top-0 md:bg-transparent ', hamburgerMenuIsOpen? '' : 'hidden')}>
-                <ul className={classNames('flex flex-col md:flex-row md:items-center h-full [&_li]:ml-6 [&_li]:border-b [&_li]:border-gray-dark md:[&_li]:border-none',
-                '[&_a]:flex [&_a]:items-center [&_a]:h-navigation-height [&_a]:w-full [&_a]:text-md md:[&_a]:text-sm [&_a:hover]:text-gray [&_a]:transition-colors '
-                )}>
-                    <li>
-                        <Link href="#">Features</Link>
-                    </li>
-                    <li>
-                        <Link href="#">Method</Link>
-                    </li>
-                    <li className="md:hidden lg:block">
-                        <Link href="#">Customers</Link>
-                    </li>
-                    <li className="md:hidden lg:block">
-                        <Link href="#">Changelog</Link>
-                    </li>
-                    <li className="md:hidden lg:block">
-                        <Link href="#">Integrations</Link>
-                    </li>
-                    <li>
-                        <Link href="#">Pricing</Link>
-                    </li>
-                    <li>
-                        <Link href="#">Company</Link>
-                    </li>
-                </ul>
-            </nav>
+            <div className={classNames(
+                'transition-[visibility]',
+                hamburgerMenuIsOpen? 'visible opacity-100' : 'invisible delay-500')} >
+                <nav className={classNames('transition-opacity duration-500 md:opacity-100 h-[calc(100vh_-_var(--navigation-height))] md:block fixed md:relative top-navigation-height left-0 bg-background w-full overflow-auto md:h-auto md:w-auto md:top-0 md:bg-transparent ',
+                hamburgerMenuIsOpen? 'visible opacity-100' : 'invisible opacity-0')}>
+                    <ul className={classNames('flex flex-col md:flex-row md:items-center h-full [&_li]:ml-6 [&_li]:border-b [&_li]:border-gray-dark md:[&_li]:border-none',
+                    '[&_a]:flex [&_a]:items-center [&_a]:h-navigation-height [&_a]:w-full [&_a]:text-md md:[&_a]:text-sm [&_a:hover]:text-gray [&_a]:transition-[color, transform] [&_a]:duration-300 [&_a]:translate-y-8 md:[&_li]:visible', hamburgerMenuIsOpen && '[&_a]:translate-y-0'
+                    )}>
+                        <li>
+                            <Link href="#">Features</Link>
+                        </li>
+                        <li>
+                            <Link href="#">Method</Link>
+                        </li>
+                        <li className="md:hidden lg:block">
+                            <Link href="#">Customers</Link>
+                        </li>
+                        <li className="md:hidden lg:block">
+                            <Link href="#">Changelog</Link>
+                        </li>
+                        <li className="md:hidden lg:block">
+                            <Link href="#">Integrations</Link>
+                        </li>
+                        <li>
+                            <Link href="#">Pricing</Link>
+                        </li>
+                        <li>
+                            <Link href="#">Company</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+           
             <div className='ml-auto flex items-center'>
                 <Link className='text-sm mr-6' href='#'>Log in</Link>
                 <Button variant="primary" href='#'>Sign up</Button>
