297da31914967c0549cceed424a521db7aab357c
McVyp <93302780+McVyp@users.noreply.github.com>
2022-12-09 16:08:52 +0545

    changed border for nav in small screen

--- components/header.tsx
+++ components/header.tsx
@@ -35,7 +35,7 @@ export const Header = () => {
           >
             <ul
               className={classNames(
-                "flex h-full flex-col md:flex-row md:items-center [&_li]:ml-6 [&_li]:border-b [&_li]:border-grey-dark md:[&_li]:border-none",
+                "flex h-full flex-col md:flex-row md:items-center [&_li]:ml-6 [&_li]:border-b [&_li]:border-white-a08 md:[&_li]:border-none",
                 "ease-in [&_a]:flex [&_a]:h-navigation-height [&_a]:w-full [&_a]:translate-y-8 [&_a]:items-center [&_a]:text-lg [&_a]:transition-[color,transform] [&_a]:duration-300 md:[&_a]:translate-y-0 md:[&_a]:text-sm [&_a]:md:transition-colors [&_a:hover]:text-grey",
                 hamburgerMenuIsOpen && "[&_a]:translate-y-0"
               )}
