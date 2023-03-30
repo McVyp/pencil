195367b97833d34de2562da13934cdb903ef9201
McVyp <93302780+McVyp@users.noreply.github.com>
2023-03-30 23:34:55 +0545

    add react-intersection-oberserver

--- components/hero-image.tsx
+++ components/hero-image.tsx
@@ -1,4 +1,7 @@
+import { useInView} from 'react-intersection-observer';
+
 export const HeroImage =() => {
+    const {} = useInView();
     return (
         <div className="[perspective:2000px] overflow-hidden mt-[12.0rem]">
             <div className="bg-hero-gradient bg-white bg-opacity-[0.01] rounded-lg [transform:rotateX(25deg)] border border-transparent-white">
