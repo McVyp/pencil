696732d9b6e4b007a32697aa03fbda2f379fe7aa
McVyp <93302780+McVyp@users.noreply.github.com>
2023-03-31 22:43:52 +0545

    add random dirt, size, durat for line animation

--- components/hero-image.tsx
+++ components/hero-image.tsx
@@ -1,27 +1,71 @@
 "use client";
 
 import classNames from 'classnames';
-import { CSSProperties, useState } from 'react';
+import { CSSProperties, useEffect, useRef, useState } from 'react';
 import { useInView} from 'react-intersection-observer';
+
+// generate ramdom numbers
+const randomNumber = (min: number, max: number) =>{
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+interface Line {
+    id: string;
+    direction: 'to bottom' | 'to right',
+    size: number,
+    duration: number
+}
+
 export const HeroImage =() => {
     const {ref, inView} = useInView({ threshold: 0.4, triggerOnce:true});
-    const [lines, setLines] = useState([
-        {direction: "to bottom", duration: 3000, size: 20},
-        {direction: "to right", duration: 3000, size: 15}
-    ])
+    const [lines, setLines] = useState<Line []>([]);
+
+    // avoid re-run of useEffect
+    const timeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+    // const [lines, setLines] = useState([
+    //     {direction: "to bottom", duration: 3000, size: 20, id: 'test'},
+    //     {direction: "to right", duration: 3000, size: 15, id: 'test'}
+    // ])
+
+    const removeLine = (id: string) =>{
+        setLines((prev) => prev.filter((line) => line.id !== id))
+    }
+
+    useEffect(()=>{
+        if(!inView) return;
+        const renderLine =(timeout: number) =>{
+        timeoutRef.current =setTimeout(()=>{
+            setLines((lines) => [
+                ...lines, {
+                direction: Math.random() > 0.5 ? "to bottom" : "to right",
+                duration: randomNumber(1300, 3500),
+                size: randomNumber(10, 30),
+                id: Math.random().toString(3).substring(7)
+            }]);
+            renderLine(randomNumber(800, 2500));
+        }, timeout)
+        };
+        renderLine(randomNumber(800, 1300));
+
+        return () =>{
+            if(timeoutRef.current) clearTimeout(timeoutRef.current);
+        }
+    },[inView, setLines])
+    
     return (
         <div
             ref={ref}
             className="[perspective:2000px] mt-[12.0rem]">
                 <div 
-                    className={classNames("bg-hero-gradient bg-white bg-opacity-[0.01] rounded-lg border border-transparent-white relative", inView ? "animate-image-rotate" : "[transform:rotateX(25deg)]",  "before:bg-hero-glow border:top-0 before:left-0 before:w-full  before:h-full before:absolute before:[filter:blur(120px)] before:opacity-0", inView && "before:animate-image-glow"
+                    className={classNames("bg-hero-gradient bg-white bg-opacity-[0.01] rounded-lg border border-transparent-white relative", inView ? "animate-image-rotate" : "[transform:rotateX(25deg)]",  "before:bg-hero-glow border:top-0 before:left-0 before:w-full before:h-full before:absolute before:[filter:blur(120px)] before:opacity-0", inView && "before:animate-image-glow"
                     
                 )}>
                     <div 
                         className='absolute w-full h-full top-0 left-0 z-20'>
                             {lines.map((line)=> (
                                  <span
-                                    key={line.duration}
+                                    onAnimationEnd={() => removeLine(line.id)}
+                                    key={line.id}
                                     style={{
                                         "--direction": line.direction,
                                         "--size": line.size,
