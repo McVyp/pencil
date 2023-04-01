30e6c25316c1b6f3a0e201f18b2ddaab039bb9bd
McVyp <93302780+McVyp@users.noreply.github.com>
2023-04-01 22:11:15 +0545

    fixed line height for tools comp

--- components/sections/any-tool.tsx
+++ components/sections/any-tool.tsx
@@ -1,7 +1,7 @@
 export const UnlikeAnyTool =() =>(
     <div className="text-white">
         <div className="text-center">
-            <h2 className="text-4xl md:text-7xl mb-4 md:mb-7">Unlike any tool
+            <h2 className="text-4xl md:text-7xl mb-4 md:mb-7 leading-4">Unlike any tool
                 <br className="hidden md:inline-block"/> you&apos;ve used before
             </h2>
             <p className="text-primary-text text-lg md:text-xl max-w-[68rem] mx-auto mb-12 md:mb-7">
