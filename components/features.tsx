eb9c13b8ca08181ace38b6ed48d1d4e3630062d0
McVyp <93302780+McVyp@users.noreply.github.com>
2023-04-09 21:05:02 +0545

    add new component in sections

--- components/features.tsx
+++ components/features.tsx
@@ -2,6 +2,10 @@ type FeaturesProps ={
     title: string;
 }
 
-export const Features = () =>{
-
+export const Features = ({title}: FeaturesProps) =>{
+    return(
+        <section>
+            <h2>{title}</h2>
+        </section>
+    )
 }
---
+++ components/sections/issue-tracking.tsx
@@ -0,0 +1,5 @@
+import { Features } from "../features"
+
+export const EnjoyIssueTracking = () => {
+    return <Features title="Issue tracking you'll enjoy using"/>;
+}
