9f7191ce42325a934c3f69ca6635159c88241479
タパ <93302780+McVyp@users.noreply.github.com>
2025-01-29 12:07:03 +0900

    Update head.tsx

--- app/head.tsx
+++ app/head.tsx
@@ -3,6 +3,7 @@ export default function Head() {
     <>
       <title>li_nea</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="theme-color" content="#000212" />
       <link rel="icon" href="/favicon.ico" />
     </>
   )
